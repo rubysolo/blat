87a7dbb19dc34a599d325240bff0f36661d4510f
Solomon White <rubysolo@gmail.com>
2015-10-29 08:58:54 -0600

    switch back to port 4000

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :blat, Blat.Endpoint,
-  http: [port: 80],
+  http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
   cache_static_lookup: false,
